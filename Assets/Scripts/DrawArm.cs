b989d1b6b294d83dcd2f5b1be66fb2825d855bbb
Laurin Muth <l.muth@lyniat.games>
2022-01-29 20:00:33 +0100

    Fix physics

--- Assets/Scripts/DrawArm.cs
+++ Assets/Scripts/DrawArm.cs
@@ -10,18 +10,35 @@ public class DrawArm : MonoBehaviour
     private Rigidbody2D end;
 
     private LineRenderer line;
+
+    [SerializeField]
+    private int LineCount;
+    
+    [SerializeField]
+    private float SinMultiplier;
+    [SerializeField]
+    private float SinLength;
+    [SerializeField]
+    private float SinTime;
     void Start()
     {
         line = GetComponent<LineRenderer>();
+        line.positionCount = LineCount;
     }
 
     // Update is called once per frame
     void Update()
     {
-        line.positionCount = 2;
-        line.SetPosition(0,start.position);
-        line.startWidth = 0.1f;
-        line.endWidth = 0.1f;
-        line.SetPosition(1,end.position);
+        for (var i = 0; i < LineCount; i++)
+        {
+            var sin = Mathf.Sin(Time.time*SinTime + i*SinLength) * SinMultiplier;
+            var posX = Mathf.Lerp(start.position.x, end.position.x, i / (float) LineCount);
+            var posY = Mathf.Lerp(start.position.y, end.position.y, i / (float) LineCount);
+            line.SetPosition(i,new Vector3(posX + sin,posY,0));
+        }
+        //line.SetPosition(0,start.position);
+        line.startWidth = 0.5f;
+        line.endWidth = 0.5f;
+        //line.SetPosition(1,end.position);
     }
 }
