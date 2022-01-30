e765edae9ff7ed4f7f9b89c6098efe04930ecd4a
Marcel Klein <Marcel-K1@gmx.net>
2022-01-30 15:20:53 +0100

    Actually

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,16 +4,34 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    //[SerializeField]
+    //Canvas curtain;
+    
     // Update is called once per frame
-    void Update()
+    //void Update()
+    //{
+    //    if (Gamepad.current.aButton.isPressed)
+    //    {
+    //        SceneManager.LoadScene(1);
+    //    }
+    //    else if (Gamepad.current.bButton.isPressed)
+    //    {
+    //        Application.Quit();
+    //    }
+    //}
+
+    public void StartGame()
     {
-        if (Gamepad.current.aButton.isPressed)
-        {
-            SceneManager.LoadScene(1);
-        }
-        else if (Gamepad.current.bButton.isPressed)
-        {
-            Application.Quit();
-        }
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Awake()
+    {
+        //Canvas curtain = GetComponent<Canvas>();
     }
 }
