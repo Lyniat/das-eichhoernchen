d90f6d083c290ff6971100c06e871ac19d4d0028
Laurin Muth <l.muth@lyniat.games>
2022-01-30 13:52:19 +0100

    Add sounds and grass

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Gamepad.current.aButton.isPressed)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else if (Gamepad.current.bButton.isPressed)
+        {
+            Application.Quit();
+        }
     }
 }
