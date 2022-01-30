471512a4d90233e420f2102a360d7b555553b488
Marcel Klein <Marcel-K1@gmx.net>
2022-01-30 12:24:34 +0100

    Main Menu

--- Assets/Scripts/Bee.cs
+++ Assets/Scripts/Bee.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 
 public class Bee : MonoBehaviour
@@ -13,20 +15,27 @@ public class Bee : MonoBehaviour
 
     [SerializeField]
     private float Speed;
+    
+    [SerializeField]
+    private float UpSpeed;
 
     private Rigidbody2D rigidbody2D;
+
+    private float up;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.position = Midpoint.position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rigidbody2D.position = Midpoint.position;
+        up += UpSpeed * Time.fixedDeltaTime;
+        //rigidbody2D.position = Midpoint.position;
         var rotPos = new Vector2(Mathf.Sin(Time.fixedTime * Speed)*RadX, Mathf.Cos(Time.fixedTime * Speed)*RadY);
         rigidbody2D.SetRotation(Mathf.Atan2(rotPos.y, rotPos.x) * Mathf.Rad2Deg);
-        rigidbody2D.position += rotPos;
+        rigidbody2D.position = rotPos + new Vector2(0,up);
     }
 }
--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -1,20 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class Camera : MonoBehaviour, IGameState
 {
-    [SerializeField]
-    private float Speed;
+    [SerializeField] private Transform target;
     // Start is called before the first frame update
+
+    private CameraState state = CameraState.Follow;
+    
+    public enum CameraState
+    {
+        Follow,
+        Idle
+    }
+    
     void Start()
     {
-        
+        GameStateObserver.getInstance().Add(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y + Speed * Time.deltaTime, transform.position.z);
+        switch (state)
+        {
+            case CameraState.Follow:
+                transform.position = new Vector3(target.position.x, target.position.y,-10);
+                break;
+            case CameraState.Idle:
+                break;
+        }
+    }
+
+    public void OnReady()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnActive()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnHit()
+    {
+        state = CameraState.Idle;
+    }
+
+    public void OnGameOver()
+    {
+        throw new System.NotImplementedException();
     }
 }
